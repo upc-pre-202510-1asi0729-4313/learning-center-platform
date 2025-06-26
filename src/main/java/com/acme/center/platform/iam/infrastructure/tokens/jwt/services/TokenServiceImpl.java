f56e9cd6906db503e4e3d8adf2d88c10c10ef6bc
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-06-25 22:24:59 -0500

    feat(iam): add token service

---
+++ src/main/java/com/acme/center/platform/iam/infrastructure/tokens/jwt/services/TokenServiceImpl.java
@@ -0,0 +1,122 @@
+package com.acme.center.platform.iam.infrastructure.tokens.jwt.services;
+
+import com.acme.center.platform.iam.infrastructure.tokens.jwt.BearerTokenService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.apache.commons.lang3.time.DateUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class TokenServiceImpl implements BearerTokenService {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(TokenServiceImpl.class);
+
+    private static final String AUTHORIZATION_PARAMETER_NAME = "Authorization";
+    private static final String BEARER_PARAMETER_PREFIX = "Bearer ";
+    private static final int TOKEN_BEGIN_INDEX = 7;
+
+    @Value("${authorization.jwt.secret}")
+    private String secret;
+
+    @Value("${authorization.jwt.expiration.days}")
+    private int expirationDays;
+
+    private String buildTokenWithDefaultParameters(String username){
+
+        var issuedAt = new Date();
+        var expiration = DateUtils.addDays(issuedAt, expirationDays);
+        var key = getSigningKey();
+
+        return Jwts.builder()
+                .subject(username)
+                .issuedAt(issuedAt)
+                .expiration(expiration)
+                .signWith(key)
+                .compact();
+
+    }
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token).getPayload();
+    }
+
+    private boolean isTokenPresentIn(String authorizationParameter) {
+        return StringUtils.hasText(authorizationParameter);
+    }
+
+    private boolean isBearerTokenIn(String authorizationParameter) {
+        return authorizationParameter.startsWith(BEARER_PARAMETER_PREFIX);
+    }
+
+    private String extractTokenFrom(String authorizationHeaderParameter) {
+        return authorizationHeaderParameter.substring(TOKEN_BEGIN_INDEX);
+    }
+
+    private String getAuthorizationParameterFrom(HttpServletRequest request) {
+        return request.getHeader(AUTHORIZATION_PARAMETER_NAME);
+    }
+
+    @Override
+    public String getBearerTokenFrom(HttpServletRequest request) {
+        String parameter = getAuthorizationParameterFrom(request);
+        if (isTokenPresentIn(parameter) && isBearerTokenIn(parameter)) return extractTokenFrom(parameter);
+        return null;
+    }
+
+    @Override
+    public String generateToken(Authentication authentication) {
+        return buildTokenWithDefaultParameters(authentication.getName());
+    }
+
+    @Override
+    public String generateToken(String username) {
+        return buildTokenWithDefaultParameters(username);
+    }
+
+    @Override
+    public String getUsernameFromToken(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    @Override
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(getSigningKey()).build().parseSignedClaims(token);
+            LOGGER.info("Token is valid");
+            return true;
+        } catch (SignatureException e) {
+            LOGGER.error("Invalid JWT signature: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            LOGGER.error("Expired JWT token: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            LOGGER.error("Unsupported JWT token: {}", e.getMessage());
+        } catch (IllegalArgumentException e) {
+            LOGGER.error("JWT claims string is empty: {}", e.getMessage());
+        }
+        return false;
+    }
+}
