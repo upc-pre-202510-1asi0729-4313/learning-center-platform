ff4bf82478bc9428ee3d2e8e33f0bf2a1e5fc551
Alberto Sanchez S <albertosanchez05@gmail.com>
2025-06-25 22:30:00 -0500

    fix(iam): update reference to entity

--- src/main/java/com/acme/center/platform/iam/infrastructure/persistence/jpa/repositories/RoleRepository.java
+++ src/main/java/com/acme/center/platform/iam/infrastructure/persistence/jpa/repositories/RoleRepository.java
@@ -1,5 +1,6 @@
 package com.acme.center.platform.iam.infrastructure.persistence.jpa.repositories;
 
+import com.acme.center.platform.iam.domain.model.entities.Role;
 import com.acme.center.platform.iam.domain.model.valueobjects.Roles;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,9 +8,9 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Roles, Long> {
+public interface RoleRepository extends JpaRepository<Role, Long> {
 
-    Optional<Roles> findByName(Roles name);
+    Optional<Role> findByName(Roles name);
 
     boolean existsByName(Roles name);
 }
